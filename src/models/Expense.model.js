385ac2612f7c304584265d5351d74e0c15c6a4c1
Dmytro Vaskiv <dimavaskiv9@gmail.com>
2026-02-28 23:20:43 +0200

    mmm

--- src/models/Expense.model.js
+++ src/models/Expense.model.js
@@ -3,28 +3,45 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../db.js');
 
-const Category = sequelize.define(
-  'Category',
+const Expense = sequelize.define(
+  'Expense',
   {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    spentAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    title: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    note: {
+      type: DataTypes.STRING,
+      defaultValue: '',
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
   },
   {
-    tableName: 'categories',
+    tableName: 'expenses',
     timestamps: false,
   }
 );
 
 module.exports = {
-  Category,
+  Expense,
 };
--- src/models/models.js
+++ src/models/models.js
@@ -1,23 +1,17 @@
 'use strict';
 
-// Імпортуємо налаштування sequelize
 const { sequelize } = require('../db.js');
-
-// 1. Імпортуємо самі моделі з їх власних файлів
 const { User } = require('./User.model.js');
 const { Expense } = require('./Expense.model.js');
 const { Category } = require('./Category.model.js');
 
-// 2. Встановлюємо зв'язки (Associations)
-// Один Користувач може мати багато Витрат
+// Встановлення зв'язків
 User.hasMany(Expense, { foreignKey: 'userId' });
 Expense.belongsTo(User, { foreignKey: 'userId' });
 
-// Одна Категорія може мати багато Витрат
 Category.hasMany(Expense, { foreignKey: 'categoryId' });
 Expense.belongsTo(Category, { foreignKey: 'categoryId' });
 
-// 3. Експортуємо моделі та sequelize для використання у тестах та сервері
 module.exports = {
   sequelize,
   models: {
